68fbba7a56a82d33733054a00c208c2e48c28563
Piotr Józefowicz <pn.jozefowicz@gmail.com>
2022-04-08 10:55:24 +0200

    .

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,7 +9,11 @@ function numberWithCommas(x) {
 }
 
 function percent_of_total(num, total) {
-    return num / total * 100
+    if (total === 0) {
+        return 0
+    } else {
+        return num / total * 100
+    }
 }
 
 function percent_change(from, to) {
@@ -28,4 +32,4 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export {roundNumber, numberWithCommas, percent_of_total, percent_change, number_after_prc_change, capitalizeFirstLetter}
+export { roundNumber, numberWithCommas, percent_of_total, percent_change, number_after_prc_change, capitalizeFirstLetter }
