35bdc8a65447d45795dcba42848c0d7c24870f9e
Piotr Józefowicz <pn.jozefowicz@gmail.com>
2022-07-09 12:28:17 +0200

    change welcome quote

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,7 @@
     <v-col class="text-center">
       <cryptoicon symbol="btc" size="128" class="ml-6"/>
       <blockquote class="blockquote">
-        &#8220;In order to use this app, u need to login bro.&#8221;
+        &#8220;In order to use this app, you need to login sir.&#8221;
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
